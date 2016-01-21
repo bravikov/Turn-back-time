aff1021994d06e16292dc5d550111a95103255a0
Dmitry Bravikov <bravikov@gmail.com>
2016-01-21 20:55:20 +0500

    Сделал отсчет точнее и надежней.

--- turn_back_time.c
+++ turn_back_time.c
@@ -1,22 +1,26 @@
 /* This program turns back time.
- * Build (Linux):
- *     gcc -o turn_back_time turn_back_time.c
- * or (Windows):
- *     i686-w64-mingw32-gcc -o turn_back_time.exe turn_back_time.c
+ * Build (for Linux):
+ *     gcc -std=c99 -o turn_back_time turn_back_time.c
+ * or (for Windows):
+ *     i686-w64-mingw32-gcc -std='c99' -static -o turn_back_time.exe turn_back_time.c
  * */
 
 #include <time.h>
 
-int main() {    
+int main() {
+    const int c = 3;
     struct timespec delay;
     delay.tv_sec = 0;
-    delay.tv_nsec = 0.9e9;
+    delay.tv_nsec = 1e9/c;
 
     while(1) {
         struct timespec tp;
         clock_gettime(CLOCK_REALTIME, &tp);
-        nanosleep(&delay, NULL);
         tp.tv_sec--;
-        clock_settime(CLOCK_REALTIME, &tp);
+        tp.tv_nsec = 0;
+        for(int i = 0; i < c; i++) {
+            clock_settime(CLOCK_REALTIME, &tp);
+            nanosleep(&delay, NULL);
+        }
     }
 }
